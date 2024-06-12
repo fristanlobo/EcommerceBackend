76e606e19163090f76c350057d55d4da5228cafd
Fristan Lobo <fristanlobo@gmail.com>
2024-06-12 23:14:58 +0530

    loggout api

---
+++ controller/userLoggout.js
@@ -0,0 +1,19 @@
+async function userLoggout(req,res) {
+    try {
+        res.clearCookie("token");
+        res.json({
+            message: "Logged out successfully",
+            error: false,
+            success: true,
+            data: []
+        })
+    }
+    catch (err) {
+        res.status(400).json({
+            message: err,
+            error: true,
+            success: false,
+        })
+    }
+}
+module.exports = userLoggout;
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,18 @@ const router = express.Router();
 
 const userSignUpController = require("../controller/userSignUp");
 const userSignInController = require("../controller/userSignIn");
-const userDetailsController=require("../controller/userDetails");
-const authToken= require('../middleware/authToken');
+const userDetailsController = require("../controller/userDetails");
+const authToken = require('../middleware/authToken');
+const userLoggout = require("../controller/userLoggout");
 
 
 router.post("/signup", userSignUpController);
 router.post("/signin", userSignInController);
-router.get('/user-details',authToken,userDetailsController)
+router.get('/user-details', authToken, userDetailsController)
+router.get("/userlogout", userLoggout)
 
-module.exports = router; 
+module.exports = router;
+
+//npm run dev
+//guravsneha@gmail.com
+//sneha123
